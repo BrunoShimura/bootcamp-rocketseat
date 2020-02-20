60d3e9c26a46ac7dd3dfc8aa292765fdd404e813
BrunoAnthonyShimura <bruno_shimura@hotmail.com>
2020-02-20 16:48:30 -0300

    first commit

---
+++ modulo01/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const server = express();
+
+
+
+server.get('/teste', (req, res) => {
+  const nome = req.query.nome;
+
+  return res.json({
+    message: `Hello ${nome}`
+  });
+})
+server.listen(3000);
